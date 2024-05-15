f64856f32d221eec0bea22b656ba7775e28c30cf
KineticCat <mysyqlabs@gmail.com>
2024-05-16 01:30:20 +0200

    small fixes, clean build

--- app/src/main/java/com/ruslan/myApplication/Exercise1Activity.kt
+++ app/src/main/java/com/ruslan/myApplication/Exercise1Activity.kt
@@ -1,11 +1,10 @@
 package com.ruslan.myApplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.ruslan.myApplication.R
 
 class Exercise1Activity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.exercies1)
+        setContentView(R.layout.homework)
     }
 }
